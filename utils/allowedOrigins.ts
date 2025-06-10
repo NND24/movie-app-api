92f341f547522d731b9e96f6ab8c5cbc6cff3717
Dat Nguyen <=>
2025-06-10 13:46:32 +0700

    chore: Update cors options

--- utils/allowedOrigins.ts
+++ utils/allowedOrigins.ts
@@ -1,1 +1,1 @@
-export const allowedOrigins = ["https://dmovie-seven.vercel.app", "http://localhost:5173", "http://localhost:4200"];
+export const allowedOrigins = ["http://localhost:4200", "https://dmovie-seven.vercel.app", "http://localhost:5173"];
